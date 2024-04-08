944731b29a72e7cba49e08d68ec1ee9155c72fe8
Rudraksh Joshi <rudrakshjoshi27@gmail.com>
2024-04-08 22:21:41 +0530

    created Booking model

---
+++ models/bookingModel.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const bookingSchema = new mongoose.Schema({
+  tour: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Tour',
+    required: [true, 'Booking must belong to a tour!'],
+  },
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: [true, 'Booking must belong to a User!'],
+  },
+  price: {
+    type: Number,
+    required: [true, 'Booking must have price'],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  paid: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+bookingSchema.pre(/^find/, function (next) {
+  this.populate({path : 'tour'});
+  this.populate({path : 'user',select : "Name"});
+  next();
+});
+
+const Booking = new mongoose.model('Booking', bookingSchema);
+module.exports = Booking;
